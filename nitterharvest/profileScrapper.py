2445c0180249f5a28363e82578420b717b59beb6
fathur <fathur@MacBook-Air-mom.local>
2023-08-14 14:52:05 +0700

    add html elements utils

--- nitterharvest/profileScrapper.py
+++ nitterharvest/profileScrapper.py
@@ -1,12 +1,14 @@
+# utils
 from utils.webdriver import start_webdriver
-from bs4 import BeautifulSoup
+from utils.html_element import LOAD_MORE, TWEET
+
+# webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
-class HTML:
-    SHOW_MORE_BUTTON = "//div[@class='show-more']"
-    TWEET_TEXT = ".tweet-content"
+# scrapper
+from bs4 import BeautifulSoup
 
 
 def profile_tweets(username: str, limit: int = 100) -> list:
@@ -30,13 +32,30 @@ def profile_tweets(username: str, limit: int = 100) -> list:
     try:
         while True:
             # Try to find the "show more" button and click it
-            load_more_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, HTML.SHOW_MORE_BUTTON)))
+            load_more_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, LOAD_MORE.BUTTON)))
             
+            # Parser HTML page
             soup = BeautifulSoup(driver.page_source, 'html.parser')
-            tweets = soup.select(HTML.TWEET_TEXT)
+            
+            tweets = soup.find_all('div', class_=TWEET.CONTAINER)
             for tweet in tweets:
-                tweets_corpus.append(tweet.get_text())
-                
+                try:
+                    tweets_corpus.append(tweet.get_text())
+                    tweet_text = tweet.find('div', class_=TWEET.TEXT).get_text() # Scrapped tweet text
+                    
+                    tweet_time = tweet.find('span', class_=TWEET.TIME).find('a')['title'] # Scrapped time
+                    
+                    tweets_corpus.append(
+                        {
+                            'time':tweet_time,
+                            'tweet':tweet_text
+                        }
+                    ) # Append data to tweets_corpus: list
+                    
+                # Skip first tweet component after n+1 pages, beacuse its not containing scrapped element
+                except AttributeError:
+                    continue 
+                    
             # click show more / next page
             load_more_button.click()
             print(f"Tweets scraped: {len(tweets_corpus)}")
@@ -53,4 +72,3 @@ def profile_tweets(username: str, limit: int = 100) -> list:
         print("Finished loading all content or an error occurred:", str(_))
         
     return tweets_corpus
-
---
+++ nitterharvest/utils/html_element.py
@@ -0,0 +1,12 @@
+class LOAD_MORE:
+    BUTTON = "//div[@class='show-more']"
+    
+class SEARCH:
+    FIELD = "//input[@name='q']"
+    BUTTON = "//button[@type='submit']"
+    
+class TWEET:
+    CONTAINER = "timeline-item"
+    TEXT = "tweet-content"
+    USERNAME = "username"
+    TIME = "tweet-date"
